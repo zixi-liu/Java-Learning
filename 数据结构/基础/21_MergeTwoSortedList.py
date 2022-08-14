a513dbf47cba9b3655c156be8d321777ade5f3ba
Zixi Liu <46979228+zixi-liu@users.noreply.github.com>
2022-08-13 17:51:42 -0700

    Create 21_MergeTwoSortedList.py

---
+++ 数据结构/基础/21_MergeTwoSortedList.py
@@ -0,0 +1,23 @@
+# 两个有序的链表合并
+
+# Time complexity : O(n + m)
+# Space complexity : O(n + m)
+
+# Use Recursion
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if list1 is None:
+            return list2
+        elif list2 is None:
+            return list1
+        elif list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
